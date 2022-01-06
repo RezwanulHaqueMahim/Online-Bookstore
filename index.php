4f347bd2e243512b9607994e7e31f4f5c73e023d
RezwanulHaqueMahim <86912829+RezwanulHaqueMahim@users.noreply.github.com>
2022-01-06 22:54:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Hello world</h1>
+<?php
+
+$x = 19;
+$y = 11;
+$z = $x+$y;
+
+echo "<h1 color: red;> $z <h1/>";
+
+
+?>
+
+</body>
+</html>
